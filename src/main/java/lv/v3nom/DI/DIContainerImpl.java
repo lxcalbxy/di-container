3abe05fd6f6b37202e95fbf875ea6c7ccd7efb7e
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-02 18:41:57 +0300

    feat: implemented resolve method logic

--- src/main/java/lv/v3nom/DI/DIContainerImpl.java
+++ src/main/java/lv/v3nom/DI/DIContainerImpl.java
@@ -1,5 +1,7 @@
 package lv.v3nom.DI;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +13,37 @@ public class DIContainerImpl implements DIContainer {
     }
 
     public <T> T resolve(Class<T> type) {
+        Class<?> implementation = dependencies.get(type);
 
+        if (implementation == null) {
+            implementation = type;
+        }
+
+        try {
+            Constructor[] constructors = implementation.getDeclaredConstructors();
+            Constructor<?> targetConstructor = constructors[0];
+
+            for (Constructor<?> c : constructors) {
+                if (c.getParameterCount() > targetConstructor.getParameterCount()) {
+                    targetConstructor = c;
+                }
+            }
+
+            Class<?>[] targetConstructorParams = targetConstructor.getParameterTypes();
+            Object[] dependencyInstances = new Object[targetConstructorParams.length];
+
+            for (int i = 0; i < targetConstructorParams.length; i++) {
+                dependencyInstances[i] = resolve(targetConstructorParams[i]);
+            }
+
+            return (T) targetConstructor.newInstance(dependencyInstances);
+
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
